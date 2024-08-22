fff971095235db83d1e27a2306cd0eeeffdb8def
ppoliani <ppoliani@gmail.com>
2024-08-22 10:18:18 +0300

    chore: update vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import EnvironmentPlugin from "vite-plugin-environment"
+import {nodePolyfills} from "vite-plugin-node-polyfills"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    nodePolyfills(),
+    react(),
+    EnvironmentPlugin('all'),
+  ]
 })
