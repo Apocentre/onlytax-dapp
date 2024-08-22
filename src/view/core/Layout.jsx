8e03f702b0611067980e4ab991ee5cc9928ce9e1
ppoliani <ppoliani@gmail.com>
2024-08-22 17:06:07 +0300

    feat: add footer

--- src/view/core/Layout.jsx
+++ src/view/core/Layout.jsx
@@ -1,7 +1,15 @@
+import Footer from "../components/Footer";
 const Layout = (props) => {
   return (
-    <div className="bg-base-100 h-screen grid grid-cols-1 place-content-center bg-[url('../../assets/images/noise.svg')]">
-      {props.children}
+    <div className="bg-base-100 h-screen flex flex-col w-screen">
+      {/* <header class="bg-white">Header</header> */}
+      <div id="header">Header</div>
+      <div id="main" class="grid grid-cols-1 flex-grow place-content-center">
+        {props.children}
+      </div>
+      <div id="footer">
+        <Footer />
+      </div>
     </div>
   );
 }
